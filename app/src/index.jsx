fc39cd827350e66a6038b7d48e52c36357ac6fd5
flesage-000 <flesage.spam@gmail.com>
2022-12-30 16:20:36 +0100

    Add Route

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -1,13 +1,22 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './pages/Home/App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
-import './index.css';
+import App from './pages/Home/App';
+import About from './pages/About/About';
+import Error from './pages/404/404';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/About' element={<About />} />
+        <Route path='/404' element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
---
+++ app/src/pages/404/404.jsx
@@ -0,0 +1,9 @@
+import './404.css';
+
+function Error() {
+  return(
+    <p>404</p>
+  )
+}
+
+export default Error;
