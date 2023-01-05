c31e3468cffe691b2d32f7099aa1c3e192af7d24
flesage-000 <flesage.spam@gmail.com>
2023-01-05 09:44:40 +0100

    Melt of corrections

--- app/src/pages/About/About.jsx
+++ app/src/pages/About/About.jsx
@@ -6,25 +6,27 @@ import Footer from '../../components/Footer/Footer';
 
 function About() {
   return(
-    <section className="App">
+    <div className="App">
       <Header />
-      <Accordion name="TestList" contentText="" contentList={[
-			"Équipements de base",
-			"Micro-Ondes",
-			"Douche italienne",
-			"Frigo",
-			"WIFI"
-		]} open={false} />
+      <div class="about">
+        <Accordion name="TestList" contentText="" contentList={[
+        "Équipements de base",
+        "Micro-Ondes",
+        "Douche italienne",
+        "Frigo",
+        "WIFI"
+      ]} open={false} />
 
-      <Accordion name="Fiabilité" contentText="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes." contentList={null} open={false} />
+        <Accordion name="Fiabilité" contentText="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes." contentList={null} open={false} />
 
-      <Accordion name="Respect" contentText="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." contentList={null} open={false} />
+        <Accordion name="Respect" contentText="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." contentList={null} open={false} />
 
-      <Accordion name="Service" contentText="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question." contentList={null} open={false} />
+        <Accordion name="Service" contentText="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question." contentList={null} open={false} />
 
-      <Accordion name="Sécurité" contentText="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes." contentList={null} open={false} />
+        <Accordion name="Sécurité" contentText="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes." contentList={null} open={false} />
+      </div>
       <Footer />
-    </section>
+    </div>
   )
 }
 
