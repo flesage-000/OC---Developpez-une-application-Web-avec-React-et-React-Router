905c93bf7ef60d70d2872b061a3ac7a2f039d55b
flesage-000 <flesage.spam@gmail.com>
2023-01-09 16:51:07 +0100

    Manage 404 erro for Kasa pages

--- app/src/pages/Kasa/Kasa.jsx
+++ app/src/pages/Kasa/Kasa.jsx
@@ -6,59 +6,63 @@ import logements from '../../../public/data/logements.json';
 import './Kasa.css';
 
 import Tags from "../../components/Tags/Tags"
-import Thumbs from '../../components/Thumbs/Thumbs';
 import Accordion from '../../components/Accordion/Accordion';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 import Carousel from '../../components/Carousel/Carousel';
-import Cover from '../../components/Cover/Cover';
 import Rating from '../../components/Rating/Rating';
 import Hostname from '../../components/Hostname/Hostname';
+import Error from '../Error/Error';
 
 function Kasa() {
   const { kasaId } = useParams();
   const kasa = logements.filter(data => data.id == kasaId)[0];
+// console.log("404?", kasa);
+  let content = '';
+  if (kasa !== undefined) {
+    content = <div className="App">
+    <Header />
 
-  return (
-    <div className="App">
-      <Header />
+    <div className="Kasa">
 
-      <div className="Kasa">
+      <div className="description_primary">
 
-        <div className="description_primary">
+        <h1>{ kasa.title }</h1>
 
-          <h1>{ kasa.title }</h1>
+        <p>
+          <small>{ kasa.location }</small>
+        </p>
 
-          <p>
-            <small>{ kasa.location }</small>
-          </p>
-
-          <Tags tags={ kasa.tags } />
-
-        </div>
-
-        <div className="description_host">
-
-          <Hostname names={ kasa.host.name } />
-
-          <div className="avatar">
-            <img src={ kasa.host.picture } alt="Avatar de `${ kasa.host.name }`" title={ kasa.host.name } />
-          </div>
-
-          <Rating rate={ kasa.rating } />
-
-        </div>
-
-        <div className="accordions">
-          <Accordion name="Description" contentText={ kasa.description } />
-          <Accordion name="équipements" contentList={ kasa.equipments } />
-        </div>
+        <Tags tags={ kasa.tags } />
 
       </div>
 
-      <Footer />
+      <div className="description_host">
+
+        <Hostname names={ kasa.host.name } />
+
+        <div className="avatar">
+          <img src={ kasa.host.picture } alt="Avatar de `${ kasa.host.name }`" title={ kasa.host.name } />
+        </div>
+
+        <Rating rate={ kasa.rating } />
+
+      </div>
+
+      <div className="accordions">
+        <Accordion name="Description" contentText={ kasa.description } />
+        <Accordion name="équipements" contentList={ kasa.equipments } />
+      </div>
+
     </div>
-  );
+
+    <Footer />
+  </div>;
+  } else {
+    content = <Error />;
+  }
+
+  return (content);
 }
 
 export default Kasa;
