a0e9bcb0276ab32eb74ea291a84706cf8d61038a
flesage-000 <flesage.spam@gmail.com>
2023-01-10 17:03:02 +0100

    Some debugs

--- app/src/components/Thumbs/Thumbs.jsx
+++ app/src/components/Thumbs/Thumbs.jsx
@@ -1,10 +1,16 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './Thumbs.css';
 
 function Thumbs({ data }) {
+  const navigation = useNavigate();
+  const navigate = () => {
+    navigation(`/Kasa/${data.id}`);
+  }
+
   return (
   <div  className="thumb"
-        style={{ "--thumb-bkg": "url(" + data.cover + ")"}}>
+        style={{ "--thumb-bkg": "url(" + data.cover + ")"}}
+        onClick={ navigate }>
     <h2><Link to={`/Kasa/${data.id}`}>{ data.title }</Link></h2>
   </div>)
 }
