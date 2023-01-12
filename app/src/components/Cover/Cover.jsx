4a236c375c770d0821351b23028e02678a19222d
flesage-000 <flesage.spam@gmail.com>
2023-01-12 16:08:41 +0100

    Removes prop-types

--- app/src/components/Cover/Cover.jsx
+++ app/src/components/Cover/Cover.jsx
@@ -1,13 +1,7 @@
-import PropTypes from 'prop-types';
-
 import "./Cover.css";
 
 function Cover({ url }) {
   return (<div className="cover" style={{ "--cover":`url(${ url })`}}></div>)
 }
-
-Cover.propTypes = {
-  url: PropTypes.string,
-};
 
 export default Cover;
